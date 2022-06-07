5d2a59124dc0e4586840b9ce8cdb48122bf42473
Zaid Shaikh <80575794+Zaid5775@users.noreply.github.com>
2022-06-07 15:59:54 +0530

    Add files via upload

---
+++ duplicate_element_in_array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cout<< "Enter n"<< endl;
+    cin>> n;
+    int arr[n];
+    cout<< "Enter elements of an array: "<< endl;
+    for(int i=0; i<n; i++){
+        cin>> arr[i];
+    }
+
+
+    cout << "Original array: ";
+    
+    for (int i=0; i < n; i++) 
+    cout << arr[i] <<" ";
+    
+    cout <<"\nDuplicate elements array: ";
+    for (int i=0; i<n; i++)
+    {
+        int j;
+        for (j=0; j<i; j++)
+           if (arr[i] == arr[j])
+           cout<< arr[i] << " ";
+           
+        //  if (i == j)
+        //   cout << arr[j] << " ";
+    }
+    return 0; 
+}
